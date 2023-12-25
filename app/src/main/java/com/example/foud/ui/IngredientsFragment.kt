4aed38e212da3ff865a499525cf0a9bea6726e73
putridriyani <114595104+putridriyani@users.noreply.github.com>
2023-12-25 22:58:14 +0700

    Update IngredientsFragment.kt

--- app/src/main/java/com/example/foud/ui/IngredientsFragment.kt
+++ app/src/main/java/com/example/foud/ui/IngredientsFragment.kt
@@ -1,17 +1,13 @@
 package com.example.foud.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.foud.R
 import com.example.foud.adapter.IngredientsAdapter
-import com.example.foud.adapter.RecipesAdapter
 import com.example.foud.databinding.FragmentIngredientsBinding
-import com.example.foud.databinding.FragmentOverviewBinding
 import com.example.foud.models.ExtendedIngredient
 import com.example.foud.models.Result
 
